1e48cbcd7af5761456a57a24f02d1523e9882919
孙良荣 <2531636478@qq.com>
2023-08-09 10:16:22 +0800

    配置npm 发布

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,5 +1,7 @@
 import { DefaultOptons, Options, TrackerConfig } from '../types/index'
 import { createHistoryEvent } from '../utils/pv'
+
+const MouseEventList: string[] = ['click', 'dblclick', 'contextmenu', 'mousedown', 'mouseup', 'mouseenter', 'mouseout', 'mouseover']
 export default class Tracker {
   private data: Options;
   constructor(options: Options) {
@@ -28,12 +30,46 @@ export default class Tracker {
   public sendReport<T>(data: T){
     this.reportTracker(data)
   }
+  // 监听js的错误
+  private errorEvent(){
+    // 监听window的error事件
+    window.addEventListener('error', (e: any) => {
+      this.reportTracker({event: 'jsError', targetKey: 'message', message: e.message})
+    })
+  }
+  // 监听promise的错误
+  private promiseError(){
+    // 监听window的unhandledrejection事件
+    window.addEventListener('unhandledrejection', (e: any) => {
+      this.reportTracker({event: 'promiseError', targetKey: 'message', message: e})
+    })
+  }
+  // 监听dom的变化
+  private targetKeyReport(){
+    //将监听的事件绑定到window上
+    MouseEventList.forEach(event => {
+      //监听document的事件
+      window.addEventListener(event,(e: any) =>{
+        //获取到当前的dom节点
+        const target = e.target as HTMLElement
+        //获取到当前dom节点的target-key属性
+        const targetValue = target.getAttribute('target-key')
+        //如果有这个属性
+        if(targetValue){
+          console.log(targetValue, this.sendReport, '监听到了')
+          //上报数据
+          this.sendReport({targetKey: targetValue, event})
+        }
+        
+      })
+    })
+  }
   private captureEvents <T>(mouseEventList: string[], keyTracker: string, data?: T){
     //将监听的事件绑定到window上
     mouseEventList.forEach(event => {
       window.addEventListener(event, function(this: any){
         console.log(event, '监听到了')
-        this.reportTracker(data)
+        this.reportTracker({data, event, targetKey: keyTracker})
       })
     })
   }
@@ -48,6 +84,17 @@ export default class Tracker {
     if(this.data.hashTracker){
       //监听hash的变化
       this.captureEvents(['hashchange'], 'hash-v')
+    }
+    // 如果开启了dom监听
+    if(this.data.domTracker){
+      //监听dom的变化
+      this.targetKeyReport()
+    }
+    // 如果开启了js错误监听
+    if(this.data.jsError){
+      //监听js的错误
+      this.errorEvent()
+      this.promiseError()
     }
   }
   private reportTracker<T>(data: T){
